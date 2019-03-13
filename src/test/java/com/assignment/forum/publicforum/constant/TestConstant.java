125c639a82d1b27da99fd29e66f79ed8e9424384
Vibhor Pandey <vibhor.vibhu@gmail.com>
2019-03-13 20:13:52 +0530

    Post Comment Unit Test Case

--- src/test/java/com/assignment/forum/publicforum/constant/TestConstant.java
+++ src/test/java/com/assignment/forum/publicforum/constant/TestConstant.java
@@ -1,14 +1,16 @@
 package com.assignment.forum.publicforum.constant;
 
-
 public abstract class TestConstant {
-    
-    public static final String TEST_USER_NAME = "Test Forum";
-    public static final String TEST_USER_EMAIL = "test@gmail.com";
-    public static final String TEST_USER_ARTICLE_CONTENT = "My name is Test. This is my post";
-    
-    public static final int DEFAULT_USER_ID = 0;
-    public static final String TEST_USER_AUTH_TOKEN = "fa0310c1ff80b43a7c8e00c03ef986b8";
-    
-    public static final String WRONG_TEST_USER_EMAIL = "tes@gmail.com";
+
+    public static final String  TEST_USER_NAME             = "Test Forum";
+    public static final String  TEST_USER_EMAIL            = "test@gmail.com";
+    public static final String  TEST_USER_ARTICLE_CONTENT  = "My name is Test. This is my post";
+    public static final String  TEST_COMMENT               = "This is Test Comment";
+
+    public static final int     DEFAULT_USER_ID            = 0;
+    public static final String  TEST_USER_AUTH_TOKEN       = "fa0310c1ff80b43a7c8e00c03ef986b8";
+    public static final String  WRONG_TEST_USER_AUTH_TOKEN = "fa0311c1ff80b45a7c8e00c03ef986b4";
+
+    public static final String  WRONG_TEST_USER_EMAIL      = "wrongtest@gmail.com";
+    public static final Integer TEST_ARTICLE_ID            = 150;
 }
--- src/test/java/com/assignment/forum/publicforum/controller/PostControllerTest.java
+++ src/test/java/com/assignment/forum/publicforum/controller/PostControllerTest.java
@@ -17,13 +17,13 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static com.forum.publicforum.constant.Path.FORUM;
 import static com.forum.publicforum.constant.Path.GET_ARTICLES;
 import static com.forum.publicforum.constant.Path.POST_ARTICLE;
+import static com.forum.publicforum.constant.Path.POST_COMMENT;
 
 import com.assignment.forum.publicforum.constant.TestConstant;
 import com.assignment.forum.publicforum.util.TestUtil;
@@ -35,6 +35,7 @@ import com.forum.publicforum.model.view.ArticleView;
 import com.forum.publicforum.model.view.UserView;
 import com.forum.publicforum.req.GetArticlesRequest;
 import com.forum.publicforum.req.PostArticleRequest;
+import com.forum.publicforum.req.PostCommentRequest;
 import com.forum.publicforum.res.UserArticlesResponse;
 import com.forum.publicforum.util.ErrorCode;
 
@@ -51,6 +52,57 @@ public class PostControllerTest {
     @InjectMocks
     private PostController postController;
     
+    
+    /**
+     * 
+     * @throws JsonProcessingException
+     * @throws Exception
+     */
+    @Test
+    public void postArticleUserNotExistsTest() throws JsonProcessingException, Exception {
+      //Request Body
+        PostArticleRequest request = new PostArticleRequest();
+        request.setAuth_token(TestConstant.TEST_USER_AUTH_TOKEN);
+        request.setEmail(TestConstant.WRONG_TEST_USER_EMAIL);
+        request.setContent(TestConstant.TEST_USER_ARTICLE_CONTENT);
+        
+        mockMvc.perform(post(FORUM + POST_ARTICLE)
+                .header("Content-Type", MediaType.APPLICATION_JSON)
+                .content(TestUtil.convertRequestObjectToString(request))
+                .characterEncoding("utf-8"))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.success", Is.is(false)))
+                .andExpect(jsonPath("$.error", Is.is(ErrorCode.USER_NOT_EXIST.getCode())));
+    }
+    
+    /**
+     * 
+     * @throws JsonProcessingException
+     * @throws Exception
+     */
+    @Test
+    public void postArticleUserNotAuthenticatedTest() throws JsonProcessingException, Exception {
+      //Request Body
+        PostArticleRequest request = new PostArticleRequest();
+        request.setAuth_token(TestConstant.WRONG_TEST_USER_AUTH_TOKEN);
+        request.setEmail(TestConstant.TEST_USER_EMAIL);
+        request.setContent(TestConstant.TEST_USER_ARTICLE_CONTENT);
+        
+        mockMvc.perform(post(FORUM + POST_ARTICLE)
+                .header("Content-Type", MediaType.APPLICATION_JSON)
+                .content(TestUtil.convertRequestObjectToString(request))
+                .characterEncoding("utf-8"))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.success", Is.is(false)))
+                .andExpect(jsonPath("$.error", Is.is(ErrorCode.USER_NOT_AUTHENTICATED.getCode())));
+    }
+    
+    
+    /**
+     * 
+     * @throws JsonProcessingException
+     * @throws Exception
+     */
     @Test
     public void postArticleSuccessTest() throws JsonProcessingException, Exception {
         
@@ -68,6 +120,37 @@ public class PostControllerTest {
                 .andExpect(jsonPath("$.success", Is.is(true)));
     }
     
+    
+    /**
+     * 
+     * @throws JsonProcessingException
+     * @throws Exception
+     */
+    @Test
+    public void postArticleCommentUserNotExistsTest() throws JsonProcessingException, Exception {
+      //Request Body
+        PostCommentRequest request = new PostCommentRequest();
+        request.setAuth_token(TestConstant.TEST_USER_AUTH_TOKEN);
+        request.setEmail(TestConstant.WRONG_TEST_USER_EMAIL);
+        request.setComment(TestConstant.TEST_COMMENT);
+        request.setArticleId(TestConstant.TEST_ARTICLE_ID);
+        
+        mockMvc.perform(post(FORUM + POST_COMMENT)
+                .header("Content-Type", MediaType.APPLICATION_JSON)
+                .content(TestUtil.convertRequestObjectToString(request))
+                .characterEncoding("utf-8"))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.success", Is.is(false)))
+                .andExpect(jsonPath("$.error", Is.is(ErrorCode.USER_NOT_EXIST.getCode())));
+    }
+    
+    
+    
+    /**
+     * 
+     * @throws JsonProcessingException
+     * @throws Exception
+     */
     @Test
     public void getArticlesUserNotExistsTest() throws JsonProcessingException, Exception {
         GetArticlesRequest request = new GetArticlesRequest();
@@ -82,8 +165,6 @@ public class PostControllerTest {
                 .andExpect(jsonPath("$.success", Is.is(false)))
                 .andExpect(jsonPath("$.error", Is.is(ErrorCode.USER_NOT_EXIST.getCode())));
     }
-    
-    
     
     /**
      * @throws Exception
