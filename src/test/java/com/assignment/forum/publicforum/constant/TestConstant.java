8412487ee0be6e401c1e3b2caa1f6c79c933310d
Vibhor Pandey <vibhor.vibhu@gmail.com>
2019-03-13 12:17:21 +0530

    Controller Unit Test Integration

---
+++ src/test/java/com/assignment/forum/publicforum/constant/TestConstant.java
@@ -0,0 +1,11 @@
+package com.assignment.forum.publicforum.constant;
+
+
+public abstract class TestConstant {
+    
+    public static final String TEST_USER_NAME = "Test Forum";
+    public static final String TEST_USER_EMAIL = "test@gmail.com";
+    public static final String TEST_USER_ARTICLE_CONTENT = "My name is Test. This is my First post";
+    
+    public static final int DEFAULT_USER_ID = 0;
+}
--- src/test/java/com/assignment/forum/publicforum/controller/PostControllerTest.java
+++ src/test/java/com/assignment/forum/publicforum/controller/PostControllerTest.java
@@ -1,9 +1,10 @@
-package com.assignment.forum.publicforum;
+package com.assignment.forum.publicforum.controller;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.hamcrest.core.Is;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,20 +16,18 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
-import static org.hamcrest.core.Is.is;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 import static com.forum.publicforum.constant.Path.HOST;
 import static com.forum.publicforum.constant.Path.FORUM;
 import static com.forum.publicforum.constant.Path.GET_ARTICLES;
 import static com.forum.publicforum.constant.Path.PAGING;
 
+import com.assignment.forum.publicforum.constant.TestConstant;
 import com.forum.publicforum.controller.PostController;
-import com.forum.publicforum.controller.UserController;
 import com.forum.publicforum.model.Comment;
 import com.forum.publicforum.model.view.ArticleView;
 import com.forum.publicforum.model.view.UserView;
@@ -38,83 +37,72 @@ import com.forum.publicforum.res.UserArticlesResponse;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(value = PostController.class)
-public class PublicForumTest {
+public class PostControllerTest {
     
     @Autowired
     private MockMvc mockMvc;
     
     @MockBean
-    private UserController userController;
-    
-    @MockBean
     private PostController postController;
     
+    /**
+     * 
+     * @throws Exception
+     */
     @Test
     public void getOwnArticles() throws Exception {
         
         //Response Body
         ArticleView articleView = new ArticleView();
-        articleView.setArticleId(149);
-        articleView.setContent("My name is Test. This is my First post");
-        articleView.setCreateTS(System.currentTimeMillis());
+        articleView.setContent(TestConstant.TEST_USER_ARTICLE_CONTENT);
         articleView.setImageUrl(null);
-        articleView.setUser(new UserView(147, "Test Forum"));
+        articleView.setUser(new UserView(TestConstant.DEFAULT_USER_ID, TestConstant.TEST_USER_NAME));
         List<Comment> comments = new ArrayList<Comment>();
         articleView.setComments(comments);
-        
         List<ArticleView> articleViews = Arrays.asList(articleView);
+        
         UserArticlesResponse articlesResponse = new UserArticlesResponse();
         articlesResponse.setArticles(articleViews);
-        articlesResponse.setSuccess(true);
        
         ResponseEntity<BaseResponse> responseEntity = new ResponseEntity<>(articlesResponse, HttpStatus.FOUND);
         
         // Request Body
         GetArticlesRequest request = new GetArticlesRequest();
-        request.setEmail("test@gmail.com");
+        request.setEmail(TestConstant.TEST_USER_EMAIL);
         request.setOwnArticles(true);
         
         given(postController.getUserArticles(request, null)).willReturn(responseEntity);
         
         mockMvc.perform(get(HOST + FORUM + GET_ARTICLES + PAGING)
                 .contentType(APPLICATION_JSON))
-                .andExpect(status().isOk())
+                .andExpect(status().isFound())
                 .andExpect(jsonPath("$", hasSize(1)))
-                .andExpect(jsonPath("$[0].articles", hasSize(1)));
+                .andExpect(jsonPath("$[0].content", Is.is(articleView.getContent())))
+                .andExpect(jsonPath("$[0].user.name", Is.is(articleView.getUser().getName())));
 
     }
     
     
+    /**
+     * 
+     * @throws Exception
+     */
     @Test
     public void getOthersArticles() throws Exception {
         
-        //Response Body
-        ArticleView articleView = new ArticleView();
-        articleView.setArticleId(149);
-        articleView.setContent("My name is Test. This is my First post");
-        articleView.setCreateTS(System.currentTimeMillis());
-        articleView.setImageUrl(null);
-        articleView.setUser(new UserView(147, "Test Forum"));
-        List<Comment> comments = new ArrayList<Comment>();
-        articleView.setComments(comments);
-        
-        List<ArticleView> articleViews = Arrays.asList(articleView);
-        UserArticlesResponse articlesResponse = new UserArticlesResponse();
-        articlesResponse.setArticles(articleViews);
-        articlesResponse.setSuccess(false);
-       
-        ResponseEntity<BaseResponse> responseEntity = new ResponseEntity<>(articlesResponse, HttpStatus.FOUND);
+        ResponseEntity<BaseResponse> responseEntity = new ResponseEntity<>(new UserArticlesResponse(), HttpStatus.FOUND);
         
         // Request Body
         GetArticlesRequest request = new GetArticlesRequest();
-        request.setEmail("test@gmail.com");
-        request.setOwnArticles(true);
+        request.setEmail(TestConstant.TEST_USER_EMAIL);
+        request.setOwnArticles(false);
         
         given(postController.getUserArticles(request, null)).willReturn(responseEntity);
         
         mockMvc.perform(get(HOST + FORUM + GET_ARTICLES + PAGING)
                 .contentType(APPLICATION_JSON))
-                .andExpect(status().isOk());
+                .andExpect(status().isFound())
+                .andExpect(jsonPath("$.success", Is.is(Boolean.TRUE)));
 
     }
 }
